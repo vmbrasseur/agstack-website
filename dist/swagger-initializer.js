13846bf9f8f8591ad24beb5f7225d419d8b6de9d
Mikela Jackson <mdockery@redhat.com>
2023-01-26 11:05:49 -0500

    Update swagger-initializer.js

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,10 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: [
-      {urls: "https://github.com/agstack/agstack-website/blob/main/apis/asset_registry.json", name: "Asset registry API"},
-      {urls: "https://github.com/agstack/agstack-website/blob/main/apis/user_registry.json", name: "User registry API"},
-      ]
+    url: "asset_registry.json", "user_registry.json"
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
