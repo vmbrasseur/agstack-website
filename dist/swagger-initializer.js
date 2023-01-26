7ee61a6fee2f0515b2f2d2049b5ed3e6a2be1f7a
Mikela Jackson <mdockery@redhat.com>
2023-01-26 14:44:09 -0500

    Update swagger-initializer.js

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -4,8 +4,8 @@ window.onload = function() {
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
     url: [
-      {url: "apis/asset_registry.json", name: "Asset registry"},
-      {url: "apis/user_registry.json", name: "User registry"},
+      {urls: "apis/asset_registry.json", name: "Asset registry"},
+      {urls: "apis/user_registry.json", name: "User registry"},
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
