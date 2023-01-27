a68458f915f19a435c6e3871e6ec7be7392501f1
dockerymick <mdockery@redhat.com>
2023-01-27 14:44:11 -0500

    Testing topbar

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,13 +3,21 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "apis/asset_registry.json",
+    urls: [
+      {url: "apis/asset_registry.json", name: "Asset registry"},
+      {url: "apis/user_registry.json", name: "User registry"},
+    ], 
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
       SwaggerUIBundle.presets.apis,
       SwaggerUIStandalonePreset
     ],
+    presets_config: {
+      SwaggerUIStandalonePreset: {
+        TopbarPlugin: true
+      }
+    },
     plugins: [
       SwaggerUIBundle.plugins.DownloadUrl
     ],
